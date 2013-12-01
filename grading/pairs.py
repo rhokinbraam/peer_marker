cc9339e727494d567648232d8095e0c48d7bd751
Ismail Dhorat <ismail@codiez.co.za>
2013-12-01 12:02:51 +0200

    refactor

--- grading/pairs.py
+++ grading/pairs.py
@@ -1,18 +1,27 @@
 import networkx as nx
 
-class Essays():
-  def __init__(self, essay_ids):
-    self.essay_ids = essay_ids
-    self.number_of_essays = len(essay_ids)
-    self.essay_map = dict(zip(range(0,self.number_of_essays),self.essay_ids))
-    self.graph = self.generate_graph(self.number_of_essays)
+class Graph():
+  def __init__(self, essay_count):
+    self.essay_count = essay_count
 
   def generate_graph(self,number_of_essays):
-    graph = nx.random_regular_graph(3, self.number_of_essays)
+    graph = nx.random_regular_graph(3, self.essay_count)
     return graph
 
   def average_degree_connectivity(self):
     return nx.average_degree_connectivity(self.graph)
 
-  def next_pair(self, id):
+  def no_self_loops(self):
     pass
+
+  def is_simple(self, graph):
+    pass
+
+class Essays():
+  def __init__(self, essay_ids):
+    self.essay_ids = essay_ids
+    self.number_of_essays = len(essay_ids)
+    self.essay_map = dict(zip(range(0,self.number_of_essays),self.essay_ids))
+    self.graph = Graph(self.number_of_essays)
+
+
--- grading/test.py
+++ grading/test.py
@@ -1,9 +1,9 @@
 import unittest
-from pairs import Essays
+from pairs import Essays, Graph
 import networkx as nx
 
 
-class TestPairFunctions(unittest.TestCase):
+class TestEssays(unittest.TestCase):
 
   def setUP(self):
     pass
@@ -12,9 +12,17 @@ class TestPairFunctions(unittest.TestCase):
     essays = Essays([123,456,789,101])
     self.assertEqual(essays.essay_map, {0: 123, 1: 456, 2: 789, 3: 101 })
 
+class TestGraph(unittest.TestCase):
+
+  def setUP(self):
+    pass
+
   def test_graph_average_degree_connectivity(self):
-    essays = Essays([123,456,789,101])
-    self.assertGreaterEqual(essays.average_degree_connectivity, 3)
+    graph = Graph(4)
+    self.assertGreaterEqual(graph.average_degree_connectivity, 3)
+
+  def test_no_self_loops(self):
+    pass
 
 if __name__ == '__main__':
     unittest.main()
